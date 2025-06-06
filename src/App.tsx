861d614ac5c07b372b86cf7f6bd67c2f52fb92e2
teop21 <38569427+teop23@users.noreply.github.com>
2025-06-06 08:36:24 +0200

    Add TooltipProvider to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Background from './components/Background';
 import Home from './pages/Home';
+import { TooltipProvider } from './components/ui/tooltip';
 import CreateTraits from './pages/CreateTraits';
 
 function App() {
@@ -9,12 +10,14 @@ function App() {
 
   return (
     <div className="relative h-screen text-foreground flex flex-col overflow-hidden">
-      <Background />
-      <Navbar className="h-16" />
-      <main className="flex-grow">
-        {path === '/' && <Home />}
-        {path === '/create-traits' && <CreateTraits />}
-      </main>
+      <TooltipProvider>
+        <Background />
+        <Navbar className="h-16" />
+        <main className="flex-grow">
+          {path === '/' && <Home />}
+          {path === '/create-traits' && <CreateTraits />}
+        </main>
+      </TooltipProvider>
     </div>
   );
 }
