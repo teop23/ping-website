95ab17644c6c76a1f8fd6f540a325984af999dd2
teop21 <38569427+teop23@users.noreply.github.com>
2025-06-08 09:04:10 +0200

    Updated CreateTraits.tsx

--- src/pages/CreateTraits.tsx
+++ src/pages/CreateTraits.tsx
@@ -680,7 +680,7 @@ const CreateTraits: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 p-4">
+    <div className="bg-gradient-to-br from-gray-50 to-gray-100 p-4">
       <div className="max-w-7xl mx-auto">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
