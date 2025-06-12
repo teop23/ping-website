fdeeef5a0b1680aee622874035aeb590a3e7f8c0
TeoP749 <teopintilie3@gmail.com>
2025-06-12 18:57:09 +0200

    socials

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,15 +2,15 @@
 export const CONTRACT_ADDRESS = "TO BE ANNOUNCED"; // Example Solana address
 
 // Buy link for the token
-export const BUY_LINK = "https://pump.fun/coin/TO_BE_ANNOUNCED";
+export const BUY_LINK = "#";
 
 // Social media links
 export const SOCIAL_LINKS = {
-  TWITTER: "https://twitter.com/ping_token",
-  TELEGRAM: "https://t.me/ping_token_official"
+  TWITTER: "https://x.com/i/communities/1933201526584963118",
+  TELEGRAM: "https://t.me/pingtoken"
 };
 
-// Other constants can be added heCre
+// Other constants can be added here
 export const NETWORK = "mainnet";
 export const TOKEN_SYMBOL = "PING";
 export const TOKEN_NAME = "PING Token";
