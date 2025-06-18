e96bf0607c7f446caf7224c058bf177048d51b38
TeoP749 <teopintilie3@gmail.com>
2025-06-18 23:02:38 +0200

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,8 @@
 // Contract addresses and other constants
-export const CONTRACT_ADDRESS = "TO BE ANNOUNCED"; // Example Solana address
+export const CONTRACT_ADDRESS = "EiFYrfJHuiWM6uHADtmzSKMAEzrANB9tLaLc4GhBpump"; // Example Solana address
 
 // Buy link for the token
-export const BUY_LINK = "#";
+export const BUY_LINK = "https://pump.fun/coin/EiFYrfJHuiWM6uHADtmzSKMAEzrANB9tLaLc4GhBpump";
 
 // Social media links
 export const SOCIAL_LINKS = {
@@ -15,7 +15,7 @@ export const SOCIAL_LINKS = {
 export const COUNTDOWN_TARGET = 1750280400 * 1000;
 
 // Show/hide countdown component
-export const SHOW_COUNTDOWN = true;
+export const SHOW_COUNTDOWN = false;
 
 // Randomizer settings
 export const EMPTY_TRAIT_CHANCE = 0.3; // 30% chance for no trait in a category
