d3f524405bd6b51133d19920f5101b3abe1c281d
teop21 <38569427+teop23@users.noreply.github.com>
2025-06-24 17:25:38 +0200

    Twitter Spaces Toast Notification System

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Background from './components/Background';
 import Navbar from './components/Navbar';
+import SpacesToast from './components/SpacesToast';
 import CreateTraits from './pages/CreateTraits';
 import Community from './pages/Community';
 import WatermarkTool from './pages/WatermarkTool';
@@ -11,6 +12,7 @@ function App() {
     <>
       <Background />
       <Navbar className="flex-shrink-0" />
+      <SpacesToast />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/community" element={<Community />} />
