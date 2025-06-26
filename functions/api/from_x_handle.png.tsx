adc1e11f8f03afe3d44daac8b1f58d39c2974f37
TeoP749 <teopintilie3@gmail.com>
2025-06-26 23:47:54 +0200

    Update from_x_handle.png.tsx

--- functions/api/from_x_handle.png.tsx
+++ functions/api/from_x_handle.png.tsx
@@ -42,29 +42,13 @@ export const onRequestGet: APIRoute = async ({ request }) => {
         }
 
         const userInfo: TwitterUserInfo = await response.json();
-        //example response
-        /**
-         * {
-      "username": "fuelkek",
-      "name": "Fuel",
-      "description": "I just searched your SOL address, I saw your trading history and was wondering if you are trying to be the first on-chain clown?",
-      "followers_count": 132089,
-      "following_count": 13601,
-      "tweet_count": 14783,
-      "location": "Aurahalla ",
-      "profile_image_url": "https://pbs.twimg.com/profile_images/1934895131812823040/R2c7RMTZ_400x400.jpg",
-      "profile_banner_url": "https://pbs.twimg.com/profile_banners/1518671564916629505/1732537335",
-      "created_at": "Mon Apr 25 19:21:53 +0000 2022",
-      "user_id": "1518671564916629505"
-    }
-         */
         const twitterPPUrl = userInfo.profile_image_url;
         console.log('Twitter profile picture URL:', twitterPPUrl);
         if (!twitterPPUrl) {
             return new Response('Twitter profile picture not found', { status: 404 });
         }
 
-        const redirectUrl = `https://pingonsol.com/api/pp.png?photo=${encodeURIComponent(twitterPPUrl)}${type ? `&type=${type}` : ''}`;
+        const redirectUrl = `https://pingonsol.com/api/pp.png?${type ? `type=${type}&` : ''}photo=${encodeURIComponent(twitterPPUrl)}`;
         return Response.redirect(redirectUrl, 307); // Temporary redirect with method preserved
     } catch (err) {
         return new Response(`Error resolving handle: ${err}`, { status: 500 });
