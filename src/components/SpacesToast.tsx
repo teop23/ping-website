3fc9b2eae9e51a99a185bce7e31d71b2b27ea2e2
TeoP749 <teopintilie3@gmail.com>
2025-07-02 23:28:48 +0200

    Update SpacesToast.tsx

--- src/components/SpacesToast.tsx
+++ src/components/SpacesToast.tsx
@@ -13,11 +13,11 @@ const SpacesToast: React.FC = () => {
     const checkSpacesStatus = async () => {
       console.log('here');
       //const data = await fetchSpacesStatus();
-      setIsVisible(false);
+      setIsVisible(true);
       setSpacesData({
-        live: false,
-        name: '$PING Daily Space #14',
-        link: 'https://x.com/i/spaces/1MnGnwdaorkJO',
+        live: true,
+        name: '$PING Daily Space #15',
+        link: 'https://x.com/i/spaces/1RDGlzrqOQqxL',
       });
       return;
       if (data) {
